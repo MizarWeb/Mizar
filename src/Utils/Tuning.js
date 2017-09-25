a1148f6a43dfa4ebc4e45092a5bcfc704077a2d5
Ratamiez <ratamiez@gmail.com>
2017-09-25 09:51:54 +0200

    end of sprint03

---
+++ src/Utils/Tuning.js
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Copyright 2017 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of MIZAR.
+ *
+ * MIZAR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MIZAR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MIZAR. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/***************************************
+ * Copyright 2011, 2012 GlobWeb contributors.
+ *
+ * This file is part of GlobWeb.
+ *
+ * GlobWeb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GlobWeb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GlobWeb. If not, see <http://www.gnu.org/licenses/>.
+ ***************************************/
+
+define(function () {
+    /**
+     @name Tuning
+     @class
+         Get some statistics
+     @constructor
+    */
+    var Tuning = function () {
+        var self = this;
+
+    };
+
+    /**************************************************************************************************************/
+
+    /**
+     Start measuring time
+     */
+    Tuning.prototype.start = function (name,index) {
+        var completeName = name;
+        if (typeof index !== 'undefined') { completeName+=index;}
+
+        this[completeName] = Date.now();
+    };
+
+    /**************************************************************************************************************/
+
+    /**
+     End measuring time
+     */
+    Tuning.prototype.end = function (name,index) {
+      var completeName = name;
+      var isIndex = (typeof index !== 'undefined');
+      if (isIndex) { completeName+=index;}
+
+      var time = Date.now() - this[completeName];
+      this[completeName] = time;
+
+      if (isIndex) {
+        if (typeof this["sum"+name] === 'undefined') {
+           this["sum"+name]=0;
+           this["nb"+name]=0;
+        }
+        this["sum"+name]+=time;
+        this["nb"+name]++;
+      }
+
+      return time;
+    };
+
+    /**************************************************************************************************************/
+
+    /**
+     Print stats in an HTML element
+     */
+    Tuning.prototype.print = function (name,index) {
+      var completeName = name;
+      var isIndex = (typeof index !== 'undefined');
+      if (isIndex) { completeName = name+= index; }
+      console.log("Elapsed ["+completeName+"] = "+this[completeName]);
+
+
+      if (!isIndex) {
+        if (typeof this["sum"+name] !== 'undefined') {
+          var avg = this["sum"+name] / this["nb"+name];
+          console.log( "Average ["+name+"] = " + avg + " ("+this["nb"+name]+" / "+this["sum"+name]+")");
+        }
+      }
+
+    };
+
+    /**************************************************************************************************************/
+
+    return Tuning;
+
+});
