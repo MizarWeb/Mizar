be1f0918963fc8227b3d1e665f301c5e6c301f0f
Jean-Christophe Malapert <jcmalapert@googlemail.com>
2018-04-11 13:50:04 +0200

    Adapts distance according FOV

--- src/NameResolver/DictionaryNameResolver.js
+++ src/NameResolver/DictionaryNameResolver.js
@@ -76,6 +76,116 @@ define(["jquery", "underscore-min", "../Utils/Utils", "./AbstractNameResolver","
         Utils.inherits(AbstractNameResolver, DictionaryNameResolver);
 
 
+        function _computeDistance(pt1, pt2) {
+            var pt = [parseFloat(pt2[0]) - parseFloat(pt1[0]), parseFloat(pt2[1]) - parseFloat(pt1[1])];
+            return Math.sqrt(pt[0] * pt[0] + pt[1] * pt[1]);
+        }
+
+        function _farestDistance(center, pts) {
+            var distance = 0;
+            for (var i=0; i<pts.length; i++) {
+                var newDistance = _computeDistance.call(this, center, pts[i]);
+                if( newDistance > distance) {
+                    distance = newDistance;
+                }
+            }
+            return distance;
+        }
+
+        function _computeFarestDistanceAlongLineString(center, pts) {
+            return _farestDistance.call(this, center, pts);
+        }
+
+        function _addPoint(p1, p2) {
+            return [parseFloat(p1[0])+parseFloat(p2[0]) , parseFloat(p1[1])+parseFloat(p2[1])];
+        }
+
+
+        function _computeLineStringBarycenter(pts) {
+            var center = pts[0];
+            for (var i = 1; i < pts.length; i++) {
+                center = _addPoint.call(this, center, pts[i]);
+            }
+            center = [center[0]/pts.length, center[1]/pts.length];
+            return center;
+        }
+
+        function _computeBarycenterAndDistance(geometry) {
+            var type = geometry.type;
+            var coordinates = geometry.coordinates;
+            var center;
+            var distance;
+            switch(type) {
+                case "Point":
+                    center = coordinates;
+                    distance = null;
+                    break;
+                case "LineString":
+                    center = _computeLineStringBarycenter.call(this, coordinates);
+                    distance = _computeFarestDistanceAlongLineString.call(this, center, coordinates);
+                    break;
+                case "Polygon":
+                    var exteriorRing = coordinates[0];
+                    center = _computeLineStringBarycenter.call(this, exteriorRing);
+                    distance =  _computeFarestDistanceAlongLineString.call(this, center, exteriorRing);
+                    break;
+                case "MultiPoint":
+                    center = _computeLineStringBarycenter.call(this, coordinates);
+                    distance =  _computeFarestDistanceAlongLineString.call(this, center, coordinates);
+                    break;
+                case "MultiLineString":
+                    var lineStringArray = coordinates[0];
+                    center = _computeLineStringBarycenter.call(this, lineStringArray[0]);
+                    center[0] = center[0]*lineStringArray[0].length;
+                    center[1] = center[1]*lineStringArray[0].length;
+                    var nbPts = 0;
+                    for (var i=1; i<lineStringArray.length;i++) {
+                        var tmpCenter = _computeLineStringBarycenter.call(this, lineStringArray[i]);
+                        tmpCenter[0] = tmpCenter[0]*lineStringArray[i].length;
+                        tmpCenter[1] = tmpCenter[1]*lineStringArray[i].length;
+                        nbPts = nbPts + lineStringArray[i].length;
+                        center = _addPoint(center, tmpCenter);
+                    }
+                    center[0] = center[0] / nbPts;
+                    center[1] = center[1] / nbPts;
+
+                    distance = 0;
+                    for (var i=0; i<lineStringArray.length;i++) {
+                        var newDistance = _farestDistance.call(this, center, lineStringArray[i]);
+                        if (newDistance > distance) {
+                            distance = newDistance;
+                        }
+                    }
+                    break;
+                case "MultiPolygon":
+                    var polygonArray = coordinates[0];
+                    center = _computeLineStringBarycenter.call(this, polygonArray[0][0]);
+                    center[0] = center[0]*polygonArray[0][0].length;
+                    center[1] = center[1]*polygonArray[0][0].length;
+                    var nbPts = 0;
+                    for (i=1; i<polygonArray.length;i++) {
+                        var tmpCenter = _computeLineStringBarycenter.call(this, lineStringArray[i][0]);
+                        tmpCenter[0] = tmpCenter[0]*lineStringArray[i][0].length;
+                        tmpCenter[1] = tmpCenter[1]*lineStringArray[i][0].length;
+                        nbPts = nbPts + lineStringArray[i][0].length;
+                        center = _addPoint.call(this, center, tmpCenter);
+                    }
+                    center[0] = center[0] / nbPts;
+                    center[1] = center[1] / nbPts;
+                    distance = 0;
+                    for (var i=0; i<polygonArray.length;i++) {
+                        var newDistance = _farestDistance.call(this, center, polygonArray[i][0]);
+                        if (newDistance > distance) {
+                            distance = newDistance;
+                        }
+                    }
+                    break;
+                default:
+                    throw "geometry "+type+" is not supported"
+            }
+            return [center, distance];
+        }
+
         /**
          * Queries the GeoJSON passed in parameter in the Mizar options
          * @function handle
@@ -93,13 +203,32 @@ define(["jquery", "underscore-min", "../Utils/Utils", "./AbstractNameResolver","
 
             // Planet resolver(Mars only currently)
             var feature = _.find(dictionary.features, function (f) {
+
                 var name = (f.properties.Name == undefined) ? f.properties.name : f.properties.Name;
-                return name.toLowerCase() === objectName.toLowerCase();
+                var isFound;
+                if (name == null) {
+                    isFound = false;
+                } else {
+                    isFound = name.toLowerCase() === objectName.toLowerCase()
+                }
+                return isFound;
             });
 
             if (feature) {
-                var lon = parseFloat(feature.properties.center_lon);
-                var lat = parseFloat(feature.properties.center_lat);
+                var lon;
+                var lat;
+                var distance;
+                if(feature.properties.center_lon == undefined || feature.properties.center_lat == undefined) {
+                    var centerAndDistance = _computeBarycenterAndDistance.call(this, feature.geometry);
+                    lon = parseFloat(centerAndDistance[0][0]);
+                    lat = parseFloat(centerAndDistance[0][1]);
+                    distance = centerAndDistance[1]
+                } else {
+                    lon = parseFloat(feature.properties.center_lon);
+                    lat = parseFloat(feature.properties.center_lat);
+                    distance = null;
+                }
+
                 feature.geometry.crs = {
                     type: "name",
                     properties: {
@@ -110,7 +239,20 @@ define(["jquery", "underscore-min", "../Utils/Utils", "./AbstractNameResolver","
                     searchLayer(objectName, onSuccess, onError, {features: [feature]});
                 };                
 
-                zoomTo(lon, lat, crs.getGeoideName(), zoomToCallback, {features: [feature]});
+                var fov = context.getRenderContext().getFov();
+
+                var distanceCamera;
+                if (distance == null) {
+                    distanceCamera = null;
+                } else {
+                    distance = distance > 180.0 ? 180.0 : distance;
+                    // aproximation of the distance in meters
+                    distance = 2 * Math.PI * crs.getGeoide().getRealPlanetRadius()  * distance / 360;
+                    distanceCamera = distance / Math.tan(0.5 * fov * Math.PI / 180)
+                }
+
+
+                zoomTo(lon, lat, distanceCamera, crs.getGeoideName(), zoomToCallback, {features: [feature]});
             }
             else {
                 searchLayer(objectName, onSuccess, onError);
