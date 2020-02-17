e8eeae31ce3beed622558176de3aa05be595b207
Charly <charly.mourglia@diginext.fr>
2020-02-17 10:51:19 +0100

    Tautological test

--- src/Renderer/RendererManager.js
+++ src/Renderer/RendererManager.js
@@ -89,7 +89,7 @@ define(["../Utils/Constants", "./RasterOverlayRenderer", "./GroundOverlayRendere
                 while (bucketIdx--) {
                     var bucket = buckets[bucketIdx];
                     var layer = bucket.layer;
-                    if (bucket.style.zIndex == Constants.DISPLAY.SELECTED_RASTER && bucket.style.zIndex <= Constants.DISPLAY.SELECTED_RASTER) {
+                    if (bucket.style.zIndex === Constants.DISPLAY.SELECTED_RASTER) {
                         bucket.style.zIndex = Constants.DISPLAY.DEFAULT_RASTER;
                     }
                     if (layer.getID() === selectedLayer.getID() && bucket.style.zIndex <= Constants.DISPLAY.SELECTED_RASTER) {
